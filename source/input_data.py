20c3a9a241e0da327863115246a1dd64ed27cdd3
Zhiying Chen <nicolec1999@foxmail.com>
2025-03-21 19:31:22 +0800

    feat: add calculate_distance

--- source/input_data.py
+++ source/input_data.py
@@ -1,5 +1,8 @@
-from typing import Dict
+from typing import List
+import numpy as np
+
 from source import domain_object as do
+
 
 class InputData:
     def __init__(
@@ -46,7 +49,7 @@ class InputData:
             if len(parts) < 7:
                 continue
             customer = do.Customer(
-                cust_id=str(int(parts[0]) + 1),
+                cust_id=parts[0],
                 x_cord=int(parts[1]),
                 y_cord=int(parts[2]),
                 demand=int(parts[3]),
@@ -57,3 +60,40 @@ class InputData:
             )
             customers[customer.cust_id] = customer
         self.customer_dict = customers
+
+    def calculate_distance(
+            self,
+            route_sol_lt: List[List[str]]
+    ):
+        cost = 0
+        for route in route_sol_lt:
+            # depot  到第一个点
+            depot = self.customer_dict["0"]
+            cust_id_1 = route[0]
+            cust_1 = self.customer_dict[cust_id_1]
+            cost += np.sqrt(
+                (depot.x_cord - cust_1.x_cord) ** 2 +
+                (depot.y_cord - cust_1.y_cord) ** 2
+            )
+
+            # 最后一个点回到depot
+            cust_id_2 = route[-1]
+            cust_2 = self.customer_dict[cust_id_2]
+            cost += np.sqrt(
+                (cust_2.x_cord - depot.x_cord) ** 2 +
+                (cust_2.y_cord - depot.y_cord) ** 2
+            )
+
+            # 中间
+            for i in range(len(route) - 1):
+                cust_id_1 = route[i]
+                cust_id_2 = route[i + 1]
+                cust_1 = self.customer_dict[cust_id_1]
+                cust_2 = self.customer_dict[cust_id_2]
+                dist = np.sqrt(
+                    (cust_1.x_cord - cust_2.x_cord) ** 2 +
+                    (cust_1.y_cord - cust_2.y_cord) ** 2
+                )
+                cost += dist
+
+        return cost
